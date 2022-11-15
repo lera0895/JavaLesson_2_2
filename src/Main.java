3494660539958cf8b3000592d7efc961f2356fbd
Лера <Lera290897@gmail.com>
2022-11-15 20:55:46 +0300

    Рубль за каждые 100 (Форматирование кода)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int beginnen = 100; // начальный счет
+        int beginner = 100; // начальный счет
         int transfer = 100; // сумма пополнения
         int bonus;
         if (transfer > 1000) {
             bonus = transfer / 100;
-        } else {bonus = 0; }
+        } else {
+            bonus = 0;
+        }
 
-        int end = beginnen + transfer + bonus;
+        int end = beginner + transfer + bonus;
 
-        System.out.println("Финальный счет:" + end );
+        System.out.println("Финальный счет:" + end);
         System.out.println("Бонусный счет:" + bonus);
     }
 }
